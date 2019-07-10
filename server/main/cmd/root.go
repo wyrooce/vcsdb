b211371397cd070b601643348556f1b1b7ed8e3a
wyrooce <m.u.m@chmai.ir>
2019-07-10 17:08:18 +0430

    command mvp

--- server/main/cmd/root.go
+++ server/main/cmd/root.go
@@ -1,26 +1,111 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
+  "fmt"
+  "github.com/spf13/cobra"
+  "os"
+  "mym/vcs/server/main/models"
+
 )
-
-
-var rootCmd = &cobra.Command{
-  Use:   "hugo",
-  Short: "Hugo is a very fast static site generator",
-  Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-  Run: func(cmd *cobra.Command, args []string) {
-	// Do Stuff Here
-	fmt.Println("test hugo command")
-  },
-}
 
 func Execute() {
   if err := rootCmd.Execute(); err != nil {
     fmt.Println(err)
     os.Exit(1)
   }
+}
+
+var mashhadSchemaFilePath string
+func init(){
+  // compareCmd.Flags().StringVarP(&mashhadSchemaFilePath, "source", "s", "", "Source file to read from")
+  rootCmd.AddCommand(compareCmd, versionCmd, exportCmd)
+}
+
+
+var rootCmd = &cobra.Command{
+  Use:   "vcs",
+  Short: "Hugo is a very fast static site generator",
+  Long: `A Fast and Flexible Static Site Generator built with
+                love by spf13 and friends in Go.
+                Complete documentation is available at http://hugo.spf13.com`,
+  Run: func(cmd *cobra.Command, args []string) {
+	// Do Stuff Here
+  fmt.Println(`Available Command:
+  - exp
+  - cmp
+  - version`)
+  },
+}
+
+var compareCmd = &cobra.Command{
+  Use:   "cmp",
+  Short: "Compare two schema",
+  Long: "salaam cmp command",
+  Run: func(cmd *cobra.Command, args []string) {
+    mashhadSchema := models.FetchSchema(args[0])	//file sent from Mashhad
+    db := models.GetDB()
+    var tehranSchema =  models.Schema{Name:os.Getenv("db_user")}
+
+    tehranSchema.Views = models.FetchView(db)
+    tehranSchema.Tables = models.FetchTable(db)
+    tehranSchema.Procedures = models.FetchProcedure(db)
+    tehranSchema.Functions = models.FetchFunction(db)
+    tehranSchema.Packages = models.FetchPackage(db)
+
+    list := tehranSchema.Compare(mashhadSchema)
+    for _, change := range list{
+      fmt.Println(change)
+    }
+    if len(list) == 0 {
+      fmt.Println("No Different Found.")
+    }
+
+    
+  },
+}
+
+
+var versionCmd = &cobra.Command{
+  Use:   "version",
+  Short: "Print the version number of TahlilVCSDB",
+  Long:  ``,
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("Tahlilgaran VCSDB Version 0.1.0")
+  },
+}
+
+var exportCmd = &cobra.Command{
+  Use:   "exp",
+  Short: "export schema that registerd in .env",
+  Long:  ``,
+  Run: func(cmd *cobra.Command, args []string) {
+      db := models.GetDB()
+    	var tehranSchema =  models.Schema{Name:os.Getenv("db_user")}
+
+      tehranSchema.Views = models.FetchView(db)
+      tehranSchema.Tables = models.FetchTable(db)
+      tehranSchema.Procedures = models.FetchProcedure(db)
+      tehranSchema.Functions = models.FetchFunction(db)
+      tehranSchema.Packages = models.FetchPackage(db)
+
+
+      f, err := os.Create("dbimage_"+os.Getenv("db_user")+".json")
+      if err != nil {
+          fmt.Println(err)
+          return
+      }
+      l, err := f.WriteString(tehranSchema.ToJSONString())
+      if err != nil {
+          fmt.Println(err)
+          f.Close()
+          return
+      }
+      fmt.Println(l, "bytes written successfully in", f.Name())
+
+      err = f.Close()
+      if err != nil {
+          fmt.Println(err)
+          return
+    }
+  },
 }
--- server/main/main.go
+++ server/main/main.go
@@ -1,73 +1,42 @@
 package main
 
 import (
-	"mym/vcs/server/main/models"
+	// "mym/vcs/server/main/models"
 	"mym/vcs/server/main/cmd"
 	"fmt"
 	"github.com/joho/godotenv"
-	"os"
-	"time"
+	// "os"
+	// "time"
 	// dif "github.com/yudai/gojsondiff"
 	// tst "github.com/yudai/gojsondiff/tests"
 )
 
 func main() {
 
-	start := time.Now()
-	cmd.Execute()
-
+	//start := time.Now()
+	
 	e := godotenv.Load()
 	if e != nil {
 		fmt.Print(e)
 	}
-
-	var tehranSchema =  models.Schema{Name:os.Getenv("db_user")}
 	
-	db := models.GetDB()
-	tehranSchema.Views = models.FetchView(db)
-	tehranSchema.Tables = models.FetchTable(db)
-	tehranSchema.Procedures = models.FetchProcedure(db)
-	tehranSchema.Functions = models.FetchFunction(db)
-	tehranSchema.Packages = models.FetchPackage(db)
 	
+	cmd.Execute()
 
-	mashhadSchema := models.FetchSchema("dbimage_pragg.json")	//file sent from Mashhad
-	// tehranSchema := models.FetchSchema("dbimage_pragg.json")	//file sent from Mashhad
-	// schema.DeleteTable("TBL_LEDGER_BRANCH")
-	// mshdSchema.DeleteTable("TBL_LEDGER_BRANCH")
+	// mashhadSchema := models.FetchSchema("dbimage_pragg.json")	//file sent from Mashhad
+	// // tehranSchema := models.FetchSchema("dbimage_pragg.json")	//file sent from Mashhad
+	// // schema.DeleteTable("TBL_LEDGER_BRANCH")
+	// // mshdSchema.DeleteTable("TBL_LEDGER_BRANCH")
 
-	list := tehranSchema.Compare(mashhadSchema)
+	// list := tehranSchema.Compare(mashhadSchema)
 
-	for _, change := range list{
-		fmt.Println(change)
-	}
-	if len(list) == 0 {
-		fmt.Println("No Different Found.")
-	}
-	
-
-		
-	// fmt.Println(string(js))
-
-	// f, err := os.Create("dbimage_"+os.Getenv("db_user")+".json")
-    // if err != nil {
-    //     fmt.Println(err)
-    //     return
-    // }
-    // l, err := f.WriteString(tehranSchema.ToJSONString())
-    // if err != nil {
-    //     fmt.Println(err)
-    //     f.Close()
-    //     return
-    // }
-    // fmt.Println(l, "bytes written successfully")
-    // err = f.Close()
-    // if err != nil {
-    //     fmt.Println(err)
-    //     return
+	// for _, change := range list{
+	// 	fmt.Println(change)
 	// }
-		
+	// if len(list) == 0 {
+	// 	fmt.Println("No Different Found.")
+	// }
 	
-	fmt.Println("Duration:", time.Now().Sub(start))
+
 }
 
